c6a7acf04e53207db018a4d51934706c087b87bc
Spiridonov Andrey <annie.skywalker@yandex.ru>
2023-05-07 22:41:13 +0300

    добавлен шаблон карточек

--- index.js
+++ index.js
@@ -23,4 +23,18 @@ const initialCards = [
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
-]; 
+];
+
+
+const cardsList = document.querySelector('.cards__list');
+const cardTemplate = document.querySelector('#cards__item');
+const cardsImage = cardTemplate.content.querySelector('.cards__image');
+const cardsTitle = cardTemplate.content.querySelector('.cards__title');
+
+initialCards.forEach(item => {
+  cardsImage.setAttribute('src', item.link);
+  cardsImage.setAttribute('alt', item.name);
+  cardsTitle.textContent = item.name;
+  const cardsItem = cardTemplate.content.cloneNode(true);
+  cardsList.append(cardsItem);
+})
